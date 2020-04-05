4213e70e6768cb9c91c4e909f14350bc396bf629
sec05 <58738123+sec05@users.noreply.github.com>
2020-04-05 18:31:07 -0400

    map game

---
+++ game.py
@@ -0,0 +1,13 @@
+import jstimers
+from time import sleep
+oxygenLevel = 80
+randEventsList = []
+playerEventsList = []
+class randEvent:
+   def __init__(self,value,duration,hp):#value:damage over time duration:time hp:points by player needed to beat event
+       self.value=value
+
+
+
+
+    
